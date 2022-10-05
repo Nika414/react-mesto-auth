dadbe65fc20cd3592f048467df4e1706c788af6e
Veronika Paist <nika.kuleva@yandex.ru>
2022-10-05 17:57:43 +0300

    feat:login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,13 @@
+
 export default function Login() {
     return (
-<></>
-    );
+        <div className="login auth">
+            <h3 className="login__header auth__header">Вход</h3>
+            <form className="login__form auth__form">
+                <input className="login__form-input auth__form-input" placeholder="Email" type="email"></input>
+                <input className="login__form-input auth__form-input" placeholder="Пароль" type="password"></input>
+                <button className="login__form-button auth__form-button" type="submit">Войти</button>
+            </form>
+        </div>
+            );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 export default function Register() {
     return (
-<div className="registration">
-    <h3 className="registration__header">Регистрация</h3>
-    <form className="registration__form">
-        <input className="registration__form-input" placeholder="Email" type="email"></input>
-        <input className="registration__form-input" placeholder="Пароль" type="password"></input>
-        <button className="registration__form-button" type="submit">Зарегистрироваться</button>
+<div className="registration auth">
+    <h3 className="registration__header auth__header">Регистрация</h3>
+    <form className="registration__form auth__form">
+        <input className="registration__form-input auth__form-input" placeholder="Email" type="email"></input>
+        <input className="registration__form-input auth__form-input" placeholder="Пароль" type="password"></input>
+        <button className="registration__form-button auth__form-button" type="submit">Зарегистрироваться</button>
     </form>
     <p className="registration__text">Уже зарегистрированы? <Link className="registration__link">Войти</Link></p>
 </div>
