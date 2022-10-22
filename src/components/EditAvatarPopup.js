ce477c4899f817ff5702d02e0cf23e6a762b2363
Veronika Paist <nika.kuleva@yandex.ru>
2022-10-22 20:33:23 +0300

    feat: avatarForm validation

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -1,31 +1,67 @@
 import PopupWithForm from "./PopupWithForm";
-import { useRef } from 'react';
-
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 export default function EditAvatarPopup({ isOpen, onClose, onUpdateAvatar }) {
-    const avatarRef = useRef();
+ 
 
-    
+  const {
+    register,
+    reset,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = useForm({
+    mode: "onChange",
+    reValidateMode: "onChange",
+    criteriaMode: "all",
+  });
 
-    function onSubmit(e) {
-     
-        e.preventDefault();
-        onUpdateAvatar({
-            avatar: avatarRef.current.value,
-        });
-        handleClear();
+  useEffect(() => {
+    if (isOpen) {
+      reset({ avatar: ''});
     }
+  }, [isOpen]);
 
-    function handleClear() {
-        avatarRef.current.value = '';
-    }
+  function onSubmit(data) {
+    onUpdateAvatar(data);
+  }
 
-    return (
-        <PopupWithForm onSubmit={onSubmit} onClose={onClose} isOpen={isOpen} name="avatar-edit" title="Обновить аватар" buttonText="Сохранить">
-            <label className="popup__form-label_value_avatar-link popup__form-label">
-                <input name="avatar"className="popup__form-item popup__form-item_value_avatar-link" ref={avatarRef} type="url" id="avatar"
-                    placeholder="Ссылка на твой новый аватар" minLength="2" />
-                <span className={`avatar-input-error popup__form-item-error'}`}></span>
-            </label>
-        </PopupWithForm>)
+  
+  return (
+    <PopupWithForm
+      onSubmit={handleSubmit(onSubmit)}
+      onClose={onClose}
+      isOpen={isOpen}
+      name="avatar-edit"
+      title="Обновить аватар"
+      buttonText="Сохранить"
+      isValid={isValid}
+    >
+      <label className="popup__form-label_value_avatar-link popup__form-label">
+        <input
+          {...register("avatar", {
+            required: "Поле обязательно к заполнению",
+            minLength: {
+              value: 2,
+              message: "Минимум 2 символа",
+            },
+            
+          })}
+          className="popup__form-item popup__form-item_value_avatar-link"
+
+          type="url"
+          id="avatar"
+          placeholder="Ссылка на твой новый аватар"
+          minLength="2"
+        />
+        <span
+          className={`avatar-input-error popup__form-item-error${
+            errors?.name && "popup__form-item-error_active"
+          }`}
+        >
+          {errors?.name?.message}
+        </span>
+      </label>
+    </PopupWithForm>
+  );
 }
